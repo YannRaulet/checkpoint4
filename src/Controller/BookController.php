72b84d6399961b09a62d8636f85a1c8cac690ad9
Yann Raulet <yann.raulet@hotmail.fr>
2021-02-04 12:17:47 +0100

    modify admin controller

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -11,22 +11,22 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/book")
+ * @Route("/admin")
  */
 class BookController extends AbstractController
 {
     /**
-     * @Route("/", name="book_index", methods={"GET"})
+     * @Route("/", name="admin_index", methods={"GET"})
      */
     public function index(BookRepository $bookRepository): Response
     {
-        return $this->render('book/index.html.twig', [
+        return $this->render('admin/index.html.twig', [
             'books' => $bookRepository->findAll(),
         ]);
     }
 
     /**
-     * @Route("/new", name="book_new", methods={"GET","POST"})
+     * @Route("/new", name="admin_new", methods={"GET","POST"})
      */
     public function new(Request $request): Response
     {
@@ -39,27 +39,27 @@ class BookController extends AbstractController
             $entityManager->persist($book);
             $entityManager->flush();
 
-            return $this->redirectToRoute('book_index');
+            return $this->redirectToRoute('admin_index');
         }
 
-        return $this->render('book/new.html.twig', [
+        return $this->render('admin/new.html.twig', [
             'book' => $book,
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="book_show", methods={"GET"})
+     * @Route("/{id}", name="admin_show", methods={"GET"})
      */
     public function show(Book $book): Response
     {
-        return $this->render('book/show.html.twig', [
+        return $this->render('admin/show.html.twig', [
             'book' => $book,
         ]);
     }
 
     /**
-     * @Route("/{id}/edit", name="book_edit", methods={"GET","POST"})
+     * @Route("/{id}/edit", name="admin_edit", methods={"GET","POST"})
      */
     public function edit(Request $request, Book $book): Response
     {
@@ -69,17 +69,17 @@ class BookController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
 
-            return $this->redirectToRoute('book_index');
+            return $this->redirectToRoute('admin_index');
         }
 
-        return $this->render('book/edit.html.twig', [
+        return $this->render('admin/edit.html.twig', [
             'book' => $book,
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/{id}", name="book_delete", methods={"DELETE"})
+     * @Route("/{id}", name="admin_delete", methods={"DELETE"})
      */
     public function delete(Request $request, Book $book): Response
     {
@@ -89,6 +89,6 @@ class BookController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('book_index');
+        return $this->redirectToRoute('admin_index');
     }
 }
